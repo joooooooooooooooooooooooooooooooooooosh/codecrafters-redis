a8d02e03fc2da864c4d40d66f2addd7025f6529f
Josh Harcombe <joshharc@gmail.com>
2024-02-13 15:12:57 +1100

    stage 4

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use anyhow::Result;
-use std::{
-    error::Error,
-    io::{Read, Write},
-};
+use std::error::Error;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
@@ -10,29 +7,26 @@ use tokio::{
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    match listener.accept().await {
-        Ok((stream, _addr)) => {
-            _ = handle_connection(stream)
-                .await
-                .map_err(|e| println!("error: {}", e))
+    loop {
+        match listener.accept().await {
+            Ok((stream, _addr)) => {
+                tokio::spawn(async {
+                    _ = handle_connection(stream)
+                        .await
+                        .map_err(|e| println!("error: {}", e))
+                });
+            }
+            Err(e) => println!("error: {}", e),
         }
-        Err(e) => println!("error: {}", e),
     }
-
-    Ok(())
 }
 
 async fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     let mut buf = [0; 1024];
     loop {
-        let _len = stream.read(&mut buf);
-        stream.write("+PONG\r\n".as_bytes()).await?;
+        let _len = stream.read(&mut buf).await?;
+        stream.write_all(b"+PONG\r\n").await?;
     }
-
-    Ok(())
 }
